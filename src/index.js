988ae93b996a50b6d813f7c60d956799e24cce8b
Masavi <masavi2008@hotmail.com>
2018-11-20 20:32:32 -0600

    Autenticación con JWT

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const SECRET = require('./utils/config');
+
+const PORT =  process.env.PORT || 5000;
+
+let verifyToken = (req, res, next) => {
+
+    const bearerHeader = req.headers['authorization'];
+
+    console.log(req.headers);
+
+    if (typeof bearerHeader !== 'undefined') {
+        // Dividir en cada espacio
+        const bearer = bearerHeader.split(' ');
+        console.log(bearer);
+
+        // Sacar el token
+        const bearerToken = bearer[1];
+
+        // Verificación del token
+        jwt.verify(bearerToken, SECRET.verifySecret, (err, decoded) => {
+            if (!err) {
+
+                // Incorporación del token a la petición del cliente
+                req.token = bearerToken;
+
+                // Utilizando el middleware de Next
+                next();
+            } else {
+                res.status(401).send({
+                    "message": "la autentificación falló..."
+                });
+            }
+        });
+    } else {
+        res.status(401).send({
+            "message": "no tienes permisos"
+        });
+    }
+}
+
+app.get('/', (req, res)=>{
+    res.send({
+        "menssage": "ok"
+    });
+});
+
+app.post('/api/posts', verifyToken ,(req, res) => {
+
+    res.send({
+        "message": "Post created..."
+    });
+});
+
+app.post('/api/login', (req, res) => {
+    
+    // dummy user
+    const user = {
+        "name": "Mauricio",
+        "last_name": "Saavedra",
+        "mail": "mauricio@mail.com"
+    }
+
+    //jwt.sign({user:user})
+    jwt.sign({user}, SECRET.verifySecret, ( err, token ) => {
+        res.send({
+            "message": "logged-in succesfully",
+            "userData": user,
+            "token": token
+        });
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ src/utils/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "verifySecret": "micodiguitosecretito"
+}
